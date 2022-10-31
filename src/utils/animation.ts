1f40b04acc62d203d9a7983a30b978c68e02d74d
luan magalhaes <m.agalhaes@hotmail.com>
2022-10-31 19:35:04 -0300

    update animation file

--- src/utils/animation.ts
+++ src/utils/animation.ts
@@ -1,18 +1,6 @@
 const easing = [0.6, -0.05, 0.01, 0.99];
-export const fadeIn = {
-  initial: {
-    opacity: 0,
-  },
-  animate: {
-    opacity: 1,
-    transition: {
-      duration: 0.3,
-      ease: easing,
-    },
-  },
-};
 
-export const fadeIn2 = {
+export const fadeIn = {
   initial: {
     opacity: 0,
   },
