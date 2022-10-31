91a8590ce58ed88974e82864fec7e960beb3e4bc
luan magalhaes <m.agalhaes@hotmail.com>
2022-10-31 19:37:27 -0300

    sort colors alphabetically

--- src/assets/colors/index.ts
+++ src/assets/colors/index.ts
@@ -1,12 +1,12 @@
 const COLORS = {
+  black: "#113537",
   brown: "#FF6B35",
-  solid: "#EDF2F4",
   darkBlue: "#2B2D42",
-  lightBlue: "#8D99AE",
   gray: "#6B818C",
   lightGray: "#E7E6DF",
-  black: "#113537",
   pink: "#FCEADE",
+  red: " #FF0000",
+  solid: "#EDF2F4",
 };
 
 export default COLORS;
